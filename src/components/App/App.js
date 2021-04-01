0ab98a46bb3e9ce503635554f017c463a50526e9
Franco Ortega <francopdx@gmail.com>
2021-03-31 19:13:52 -0700

    add initial *coming soon* content and styling

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+import './App.css';
+
+const App = props => {
+  return (
+    <div className="App">
+      <h1>Franco Ortega</h1>
+      <h3>Full Stack Software Engineer</h3>
+      <h4>&lowast; &lowast; &lowast; portfolio coming soon &lowast; &lowast; &lowast;</h4>
+      
+    </div>
+  );
+};
+
+// App.propTypes = {
+
+// };
+
+export default App;
