ff05db13f4b17a2a34e0384fd655de8f44aa125a
Franco Ortega <francopdx@gmail.com>
2021-04-07 14:53:51 -0700

    replace corner indents that used position with corner indents using grid

--- src/components/ocean/Ocean.jsx
+++ src/components/ocean/Ocean.jsx
@@ -6,29 +6,43 @@ const Ocean = () => {
   return (
     <div className={styles.Ocean}>
       {/* <ComingSoon /> */}
-      <div className={styles.CornerNW}></div>
-      <div className={styles.CornerNE}></div>
-      <div className={styles.CornerSW}></div>
-      <div className={styles.CornerSE}></div>
+      {/* <div className={styles.CornerNW}></div> */}
+      {/* <div className={styles.CornerNE}></div> */}
+      {/* <div className={styles.CornerSW}></div> */}
+      {/* <div className={styles.CornerSE}></div> */}
       <div className={styles.OceanGrid}>
 
-        <div className={styles.OceanGridItem}></div>
-        <div className={styles.OceanGridItem}></div>
-        <div className={`${styles.OceanGridItem} ${styles.OceanGridName}`}>
-          <p className={styles.Name}>Franco Ortega</p>
-          {/* <p className={styles.Title}>Full Stack Software Engineer</p>
-          <p className={styles.Message}>&lowast; &lowast; &lowast; portfolio coming soon &lowast; &lowast; &lowast;</p> */}
+        {/* Top Left Corner */}
+        <div className={styles.OceanGridItem}>
+          <div className={styles.CornerNW}></div>
         </div>
 
+        <div className={styles.OceanGridItem}></div>
+
+        {/* Name */}
+        <div className={`${styles.OceanGridItem} ${styles.OceanGridName}`}>
+          <p className={styles.Name}>Franco Ortega</p>
+        </div>
+
+        {/* Title */}
         <div className={`${styles.OceanGridItem} ${styles.OceanGridTitle}`}>
-          
           <p className={styles.Title}>Full Stack Software Engineer</p>
           <p className={styles.Message}>&lowast; &lowast; &lowast; portfolio coming soon &lowast; &lowast; &lowast;</p>
         </div>
 
+        <div className={`${styles.OceanGridItem} ${styles.Test}`}></div>
+        <div className={`${styles.OceanGridItem} ${styles.Test}`}></div>
         <div className={styles.OceanGridItem}></div>
         <div className={styles.OceanGridItem}></div>
         <div className={styles.OceanGridItem}></div>
+
+
+        {/* Top Right Corner */}
+        <div className={`${styles.OceanGridItem} ${styles.CornerNEContainer}`}>
+          <div className={styles.CornerNE}></div>
+        </div>
+
+
         <div className={styles.OceanGridItem}></div>
         <div className={styles.OceanGridItem}></div>
         <div className={styles.OceanGridItem}></div>
@@ -85,6 +99,12 @@ const Ocean = () => {
         <div className={styles.OceanGridItem}></div>
         <div className={styles.OceanGridItem}></div>
         <div className={styles.OceanGridItem}></div>
+
+        {/* Bottom Left Corner */}
+        <div className={`${styles.OceanGridItem} ${styles.CornerSWContainer}`}>
+          <div className={styles.CornerSW}></div>
+        </div>
+
         <div className={styles.OceanGridItem}></div>
         <div className={styles.OceanGridItem}></div>
         <div className={styles.OceanGridItem}></div>
@@ -95,11 +115,12 @@ const Ocean = () => {
         <div className={styles.OceanGridItem}></div>
         <div className={styles.OceanGridItem}></div>
         <div className={styles.OceanGridItem}></div>
-        <div className={styles.OceanGridItem}></div>
-        <div className={styles.OceanGridItem}></div>
-        <div className={styles.OceanGridItem}></div>
-        <div className={styles.OceanGridItem}></div>
-        
+
+        {/* Bottom Right Corner */}
+        <div className={`${styles.OceanGridItem} ${styles.CornerSEContainer}`}>
+          <div className={styles.CornerSE}></div>
+        </div>
+
 
       </div>
     </div>
