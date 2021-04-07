72ba1adb5bb093ab94617707d78469b288c9beaf
Franco Ortega <francopdx@gmail.com>
2021-04-06 17:13:08 -0700

    create ComingSoon component for coming soon info

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import Ocean from '../ocean/Ocean';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Franco Ortega</h1>
-      <h3>Full Stack Software Engineer</h3>
-      <h4>&lowast; &lowast; &lowast; portfolio coming soon &lowast; &lowast; &lowast;</h4>
+      
+      <Ocean />
       
     </div>
   );
--- src/components/ocean/Ocean.jsx
+++ src/components/ocean/Ocean.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ComingSoon from '../../comingSoon/ComingSoon';
+import styles from './Ocean.module.css';
 
 const Ocean = () => {
   return (
-    <div>
-            
+    <div className={styles.Ocean}>
+      <ComingSoon />
     </div>
   );
 };
