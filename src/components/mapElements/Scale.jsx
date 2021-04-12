d5e25c9002b5e5a242e038a0328e7b052a232b78
Franco Ortega <francopdx@gmail.com>
2021-04-12 13:43:33 -0700

    refactor Scale component

--- src/components/mapElements/Scale.jsx
+++ src/components/mapElements/Scale.jsx
@@ -5,37 +5,38 @@ import styles from './Scale.module.css';
 const Scale = () => {
   return (
     <main className={styles.ScaleContainer}>
-      <section className={styles.Distance}>
-        <p>
-          0
-        </p>
-        <p>
-          10
-        </p>
-        <p>
-          20
-        </p>
-        <p>
-          30
-        </p>
-        <p>
-          40
-        </p>
-        <p>
-          50
-        </p>
-        <p>
-          60
-        </p>
-      </section>
-      {/* <header className={styles.Header}>Scale</header> */}
       <section className={styles.Scale}>
-        <section className={styles.Black}></section>
-        <section className={styles.White}></section>
-        <section className={styles.Black}></section>
-        <section className={styles.White}></section>
-        <section className={styles.Black}></section>
-        <section className={styles.White}></section>
+        <section className={styles.ScaleItem}>
+          <section className={styles.ScaleNumber}>0</section>
+          <section className={`${styles.ScaleBlockBlack} ${styles.ScaleStart}`}></section>
+        </section>
+        <section className={styles.ScaleItem}>
+          <section className={styles.ScaleNumber}>10</section>
+          <section className={styles.ScaleBlockWhite}></section>
+        </section>
+        <section className={styles.ScaleItem}>
+          <section className={styles.ScaleNumber}>20</section>
+          <section className={styles.ScaleBlockBlack}></section>
+        </section>
+        <section className={styles.ScaleItem}>
+          <section className={styles.ScaleNumber}>30</section>
+          <section className={styles.ScaleBlockWhite}></section>
+        </section>
+        <section className={styles.ScaleItem}>
+          <section className={styles.ScaleNumber}>40</section>
+          <section className={styles.ScaleBlockBlack}></section>
+        </section>
+        <section className={styles.ScaleItem}>
+          <section className={styles.ScaleNumber}>50</section>
+          <section className={`${styles.ScaleBlockWhite} ${styles.ScaleEnd}`}></section>
+        </section>
+        <section className={styles.ScaleItem}>
+          <section className={styles.ScaleNumber}>60</section>
+          <section className={`${styles.ScaleBlockBlack} ${styles.ScaleEnd}`}></section>
+        </section>
+        <section className={styles.ScaleItem}>
+          <section className={styles.ScaleNumber}>70</section>
+        </section>
       </section>
 
     </main>
