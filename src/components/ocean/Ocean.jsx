2f1b9ccde737dd0f00995254d014ce2ad1c3a2ed
Franco Ortega <francopdx@gmail.com>
2021-04-13 17:24:44 -0700

    remove commented out border corner code from Ocean

--- src/components/ocean/Ocean.jsx
+++ src/components/ocean/Ocean.jsx
@@ -13,48 +13,30 @@ const Ocean = () => {
       <div className={styles.OceanGrid}>
 
         {/* Top Left Corner */}
-        {/* <div className={`${styles.OceanGridItem} ${styles.CornerNWContainer}`}>
-          <div className={styles.CornerNW}></div>
-        </div> */}
         <BorderCorner
           grid={borderCornerData.cornerNW.grid}
           shape={borderCornerData.cornerNW.shape}
         />
-
         {/* Top Right Corner */}
-        {/* <div className={`${styles.OceanGridItem} ${styles.CornerNEContainer}`}>
-          <div className={styles.CornerNE}></div>
-        </div> */}
         <BorderCorner
           grid={borderCornerData.cornerNE.grid}
           shape={borderCornerData.cornerNE.shape}
         />
-
         {/* Bottom Left Corner */}
-        {/* <div className={`${styles.OceanGridItem} ${styles.CornerSWContainer}`}>
-          <div className={styles.CornerSW}></div>
-        </div> */}
         <BorderCorner
           grid={borderCornerData.cornerSW.grid}
           shape={borderCornerData.cornerSW.shape}
         />
-
-
         {/* Bottom Right Corner */}
-        {/* <div className={`${styles.OceanGridItem} ${styles.CornerSEContainer}`}>
-          <div className={styles.CornerSE}></div>
-        </div> */}
         <BorderCorner
           grid={borderCornerData.cornerSE.grid}
           shape={borderCornerData.cornerSE.shape}
         />
 
-
         {/* Name */}
         <div className={`${styles.OceanGridItem} ${styles.OceanGridName}`}>
           <p className={styles.Name}>Franco Ortega</p>
         </div>
-
         {/* Title */}
         <div className={`${styles.OceanGridItem} ${styles.OceanGridTitle}`}>
           <p className={styles.Title}>Full Stack Software Engineer</p>
@@ -63,17 +45,15 @@ const Ocean = () => {
 
         {/* Team Projects */}
         <Projects />
-
         {/* Profile */}
         <Profile />
-
         {/* Resume */}
         <Resume />
 
         {/* Scale */}
         <Scale />
 
-        <div className={styles.OceanGridItem}></div>
+        {/* <div className={styles.OceanGridItem}></div> */}
       </div>
     </main>
   );
