1fb7afcc10a93d24f3b97cef6c1f0e1edb01e291
Franco Ortega <francopdx@gmail.com>
2021-04-13 17:49:08 -0700

    clean up code

--- src/components/ocean/Ocean.jsx
+++ src/components/ocean/Ocean.jsx
@@ -10,51 +10,45 @@ import { borderCornerData } from '../mapElements/borderCornerData';
 const Ocean = () => {
   return (
     <main className={styles.Ocean}>
-      <div className={styles.OceanGrid}>
+      <section className={styles.OceanGrid}>
 
-        {/* Top Left Corner */}
+        {/* Border Corners */}
         <BorderCorner
           grid={borderCornerData.cornerNW.grid}
           shape={borderCornerData.cornerNW.shape}
         />
-        {/* Top Right Corner */}
         <BorderCorner
           grid={borderCornerData.cornerNE.grid}
           shape={borderCornerData.cornerNE.shape}
         />
-        {/* Bottom Left Corner */}
         <BorderCorner
           grid={borderCornerData.cornerSW.grid}
           shape={borderCornerData.cornerSW.shape}
         />
-        {/* Bottom Right Corner */}
         <BorderCorner
           grid={borderCornerData.cornerSE.grid}
           shape={borderCornerData.cornerSE.shape}
         />
 
-        {/* Name */}
+        {/* Name & Title */}
         <div className={`${styles.OceanGridItem} ${styles.OceanGridName}`}>
           <p className={styles.Name}>Franco Ortega</p>
         </div>
-        {/* Title */}
         <div className={`${styles.OceanGridItem} ${styles.OceanGridTitle}`}>
           <p className={styles.Title}>Full Stack Software Engineer</p>
           <p className={styles.Message}>&lowast; &lowast; &lowast; portfolio in progress &lowast; &lowast; &lowast;</p>
         </div>
 
-        {/* Team Projects */}
+        {/* Continents */}
         <Projects />
-        {/* Profile */}
         <Profile />
-        {/* Resume */}
         <Resume />
 
         {/* Scale */}
         <Scale />
 
         {/* <div className={styles.OceanGridItem}></div> */}
-      </div>
+      </section>
     </main>
   );
 };
