56ffd03597f11ef82bc4bbb57be17fefa40d67ed
Franco Ortega <francopdx@gmail.com>
2021-04-15 16:32:16 -0700

    add GitHub island

--- src/components/islands/IslandChain.jsx
+++ src/components/islands/IslandChain.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import GitHub from './GitHub';
 import styles from './IslandChain.module.css';
 
 const IslandChain = () => {
   return (
     <main className={styles.IslandChain}>
+      <GitHub />
       <div className={styles.IslandGridBlock}></div>
       <div className={styles.IslandGridBlock}></div>
       <div className={styles.IslandGridBlock}></div>
