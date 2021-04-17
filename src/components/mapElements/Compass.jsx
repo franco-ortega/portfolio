c62dc62c1b3e3b851580b0f1726949a18687e26c
Franco Ortega <francopdx@gmail.com>
2021-04-16 18:39:09 -0700

    add compass

--- src/components/mapElements/Compass.jsx
+++ src/components/mapElements/Compass.jsx
@@ -4,12 +4,59 @@ import styles from './Compass.module.css';
 const Compass = () => {
   return (
     <main className={styles.Compass}>
+      
+
+      <section className={styles.TopTriangle}></section>
+      <section className={styles.BottomTriangle}></section>
+      <section className={styles.LeftTriangle}></section>
+      <section className={styles.RightTriangle}></section>
+        
+      {/* <div className={styles.GridItem}></div>
+        <div className={styles.GridItem}></div>
+        <div className={styles.GridItem}></div>
+        <div className={styles.GridItem}></div> */}
+
+      {/* <section className={styles.InnerTriangle}></section> */}
+
       <section className={styles.CompassCircle}>
-        <section className={styles.CompassCenter}>
-
-        </section>
-
+        <section className={styles.CompassCenter}></section>
       </section>
+
+      <section className={styles.OuterCircle}>
+        <section className={styles.MysteryCircle}></section>
+      </section>
+
+
+
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
+      <div className={styles.GridItem}></div>
     </main>
   );
 };
