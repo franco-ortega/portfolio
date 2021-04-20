45e916c5a019318ac988ed64702df2ff203ae6e7
Franco Ortega <francopdx@gmail.com>
2021-04-19 17:18:08 -0700

    adjust styling of WeatherBeats project for mobile view

--- src/components/projects/WeatherBeats/WeatherBeatsDetails.jsx
+++ src/components/projects/WeatherBeats/WeatherBeatsDetails.jsx
@@ -8,6 +8,11 @@ const WeatherBeatsDetails = () => {
     <>
       <ProjectsDetails />
       <main className={styles.WeatherBeatsDetails}>
+        <section className={styles.ButtonContainer}>
+          <Link to="/projects">
+            <button className={styles.CloseButton} aria-label="Close">X</button>
+          </Link>
+        </section>
         <h1>WeatherBeats</h1>
         <section className={styles.ProjectTech}>
         React | CSS | Node | Express | WeatherBit API | Spotify API | Heroku | Netlify
@@ -16,12 +21,9 @@ const WeatherBeatsDetails = () => {
             WeatherBeats provides a music playlist based on the weather. Users may customize their experience by selecting a preferred location, weather, or genre. Completed in a 10-day remote sprint with a 5-member team. My contributions included the acquisition of location data and associated fetch requests; creating a responsive design and consistent layout; building data files incorporated with props to enhance modularity and scalability; refactoring repetitive code.
         </section>
         <section className={styles.ProjectLinks}>
-          <a href="https://weatherbeats.netlify.app/" rel="noreferrer" target="_blank">Website</a> |
-          | <a href="https://github.com/WeatherBeats" rel="noreferrer" target="_blank">GitHub</a>
+          <a href="https://weatherbeats.netlify.app/" rel="noreferrer" target="_blank" className={styles.LinkText}>Website</a> |
+          | <a href="https://github.com/WeatherBeats" rel="noreferrer" target="_blank" className={styles.LinkText}>GitHub</a>
         </section>
-        <Link to="/projects">
-          <button className={styles.CloseButton} aria-label="Close">X</button>
-        </Link>
       </main>
     </>
   );
