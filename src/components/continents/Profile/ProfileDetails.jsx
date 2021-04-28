1ba35e4dbcb6032d5af3727eddac755303fdfdc5
Franco Ortega <francopdx@gmail.com>
2021-04-27 18:00:31 -0700

    update Profile bio

--- src/components/continents/Profile/ProfileDetails.jsx
+++ src/components/continents/Profile/ProfileDetails.jsx
@@ -28,7 +28,7 @@ const Profile = () => {
                 Adaptability, empathy, and patience are strengths that have served me well across multiple fields from publishing to education to transportation. I have applied these to brainstorming, problem solving, and maintaining forward momentum even as circumstances change unexpectedly. Furthermore, my professional experience has put me in constant contact with people of all ages and from many walks of life. This has given me tremendous opportunity to grow as a communicator, listener, and mediator. I look forward to using these strengths and experiences to enhance my contributions as a member of a software development team for a company that values the well-being of its workers and cares about its impact.
               </p>
               <p className={styles.BioParagraph}>
-                Additionally, despite being a math and science kid, I have learned to appreciate and nurture my creative side through writing, cartography, and designing boardgames. I also enjoy bouldering, getting around on bike, and attending art events.
+                Furthermore, in addition to growing up as a math and science kid, I have learned to appreciate and nurture my creative side through writing, cartography, and boardgame design. Other things that make me smile include bouldering, getting around on bike, attending art events, sharing food at potlucks, and visiting friends who have wound up in far-flung places. Also, roller hockey, punk rock, and zines were big parts of my past that I still very much appreciate and occasionally enjoy in smaller doses these days.
               </p>
               <p className={styles.BioParagraph}>
                 Yes to social justice, climate justice, and economic justice. 
