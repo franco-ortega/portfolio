e97090f80cae430f386135b4112a38139b62b3e6
Franco Ortega <francopdx@gmail.com>
2021-05-08 18:15:45 -0700

    adjust project summary containers on Project continent details

--- src/components/projects/BeHuman/BeHuman.jsx
+++ src/components/projects/BeHuman/BeHuman.jsx
@@ -5,13 +5,15 @@ import styles from './BeHuman.module.css';
 const BeHuman = () => {
   return (
     <main className={styles.BeHuman}>
-      <h2>beHuman</h2>
+      <h2 className={styles.Title}>beHuman</h2>
       <section className={styles.Summary}>
         beHuman is a VSCode extension that prompts users to take breaks with an hourly pop-up that provides wellness tips and movement videos.
       </section>
-      <Link to="/behuman">
-        <button className={styles.DetailsButton} aria-label="Project Details">Click for project details.</button>
-      </Link>
+      <section className={styles.ButtonContainer}>
+        <Link to="/behuman">
+          <button className={styles.DetailsButton} aria-label="Project Details">Click for project details.</button>
+        </Link>
+      </section>
     </main>
   );
 };
--- src/components/projects/WeatherBeats/WeatherBeats.jsx
+++ src/components/projects/WeatherBeats/WeatherBeats.jsx
@@ -5,13 +5,15 @@ import styles from './WeatherBeats.module.css';
 const WeatherBeats = () => {
   return (
     <main className={styles.WeatherBeats}>
-      <h2>WeatherBeats</h2>
+      <h2 className={styles.Title}>WeatherBeats</h2>
       <section className={styles.Summary}>
         WeatherBeats provides a music playlist based on the weather. Users may customize their experience by selecting a preferred location, weather, or genre.
       </section>
-      <Link to="/weatherbeats">
-        <button className={styles.DetailsButton} aria-label="Project Details">Click for project details.</button>
-      </Link>
+      <section className={styles.ButtonContainer}>
+        <Link to="/weatherbeats">
+          <button className={styles.DetailsButton} aria-label="Project Details">Click for project details.</button>
+        </Link>
+      </section>
     </main>
   );
 };
