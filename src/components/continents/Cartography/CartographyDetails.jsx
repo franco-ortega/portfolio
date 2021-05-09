174eb6684e794031733fd97c41749283386169c3
Franco Ortega <francopdx@gmail.com>
2021-05-09 08:12:33 -0700

    increase size of ProfileDetails and continent-details-title; change h1 tag in CartographyDetails to header tag

--- src/components/continents/Cartography/CartographyDetails.jsx
+++ src/components/continents/Cartography/CartographyDetails.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './CartographyDetails.module.css';
-// import Ganderlund from '../../../assets/maps/Ganderlund.png';
 import April from '../../../assets/maps/April.png';
 import Harland from '../../../assets/maps/Harland.png';
-// import MarneOsso from '../../../assets/maps/MarneOsso.png';
 import Strigi from '../../../assets/maps/Strigi.png';
 import Gruenne from '../../../assets/maps/Gruenne.png';
 
@@ -15,7 +13,7 @@ const CartographyDetails = () => {
         <section className={styles.InnerCoastline}>
           <section className={styles.Continent}>
             <p className={styles.Title}>
-              <h1>Cartography</h1>
+              <header className={styles.Title}>Cartography</header>
             </p>
             <p className={styles.Content}>
               The inspiration for this site came from my longtime love of fantasy cartography. From Middle Earth to Westeros to the Grishaverse, I&apos;ve always loved flipping to the maps in fantasy novels whenever a new location is mentioned, and in recent years, I&apos;ve started creating my own worlds with pen and paper. More maps can be viewed at <a href="https://www.instagram.com/reefwoodcartography/" target="_blank" rel="noreferrer">@ReefwoodCartography on Instagram</a>.
@@ -32,12 +30,6 @@ const CartographyDetails = () => {
             <p className={styles.MapGruenne}>
               <img src={Gruenne} alt="Gruenne map" className={styles.Gruenne} />
             </p>
-            {/* <p className={styles.MapGanderlund}>
-              <img src={Ganderlund} alt="Ganderlund map" className={styles.Ganderlund} />
-            </p> */}
-            {/* <p className={styles.MapMarneOsso}>
-              <img src={MarneOsso} alt="MarneOsso map" className={styles.MarneOsso} />
-            </p> */}
             <p className={styles.ButtonContainer}>
               <Link to="/">
                 <button className={styles.Button}>X</button>
