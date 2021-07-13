9ec9dd66e53a8de5b40689a116f955e26cfdcbfc
Franco Ortega <francopdx@gmail.com>
2021-07-12 18:18:02 -0700

    reshape Projects popup: desktop large view

--- src/components/continents/Projects/ProjectsDetails.jsx
+++ src/components/continents/Projects/ProjectsDetails.jsx
@@ -8,35 +8,38 @@ import styles from './ProjectsDetails.module.css';
 
 const ProjectsDetails = () => {
   return (
-    <section className={styles.ProjectsDetails}>
-      <div className={styles.OuterCoastline}>
-        <div className={styles.InnerCoastline}>
-          <div className={styles.Continent}>
-            <header className={styles.Title}>Team Projects</header>
-            <p className={styles.Intro}>
-                These projects were completed remotely with 3-5 member teams over 5-10 day sprints.
-            </p>
-            <section className={styles.ProjectsDisplayCase}>
-              <section className={styles.Project}>
-                <WeatherBeats />
-              </section>
-              <section className={styles.Project}>
-                <BeHuman />
-              </section>
-              <section className={styles.Project}>
-                <ChooseGif />
-              </section>
-              <section className={styles.Project}>
-                <MajorArcana />
-              </section>
-            </section>
-            <Link to="/">
-              <button className={styles.Button} title="Close" name="Close" aria-label="Close">X</button>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </section>
+    <main className={styles.ProjectsDetails}>
+      <Link to="/">
+        <button className={styles.Button} title="Close" name="Close" aria-label="Close">X</button>
+      </Link>
+      
+      <header className={styles.Header}>
+        <p className={styles.Title}>
+          Team Projects
+        </p>
+        <p className={styles.Intro}>
+                  All projects were completed remotely with 3-5 member teams.
+        </p>
+      </header>
+      <section className={styles.ProjectsDisplayCase}>
+        <section className={styles.Project}>
+          <WeatherBeats />
+        </section>
+        <section className={styles.Project}>
+          <BeHuman />
+        </section>
+        <section className={styles.Project}>
+          <ChooseGif />
+        </section>
+        <section className={styles.Project}>
+          <MajorArcana />
+        </section>
+        <section className={styles.Project}>
+          <MajorArcana />
+        </section>
+      </section>
+            
+    </main>
   );
 };
 
