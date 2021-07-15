192843d5e02d4f7cad23367b8897bb76f04ce3d0
Franco Ortega <francopdx@gmail.com>
2021-07-15 09:25:49 -0700

    resize MajorArcanaDetails for all screen sizes

--- src/components/projects/MajorArcana/MajorArcanaDetails.jsx
+++ src/components/projects/MajorArcana/MajorArcanaDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import ClosePopup from '../../buttons/ClosePopup';
 import ProjectsDetails from '../../continents/Projects/ProjectsDetails';
 import MajorArcanaIcon from '../../../assets/MajorArcana.png';
 import styles from './MajorArcanaDetails.module.css';
@@ -9,24 +9,25 @@ const MajorArcanaDetails = () => {
     <>
       <ProjectsDetails />
       <main className={styles.MajorArcanaDetails}>
-        <section className={styles.ButtonContainer}>
-          <Link to="/projects">
-            <button className={styles.CloseButton} aria-label="Close">X</button>
-          </Link>
-        </section>
+        <ClosePopup path={'projects'} />
         <h1 className={styles.ProjectTitle}>Major Arcana Divinator</h1>
         <h2 className={styles.ProjectTech}>HTML | CSS | JavaScript</h2>
         <section className={styles.ProjectInfo}>
         Browser-based app that allows the user to select a one-card or three-card tarot reading. The cards of a reading and the corresponding timestamp are saved in local storage. The user may look back on their readings or delete them. My responsibilities focused on creating and displaying the time stamp as well as rendering the cards from local storage onto the browser.
         </section>
+        <img
+          src={MajorArcanaIcon}
+          alt="Rotating cube that displays a gif on each side."
+          className={styles.ProjectIcon}
+        />
         <section className={styles.ProjectLinks}>
-          <a href="https://mystechal-divinators.github.io/project-week-tarot-divinators/" rel="noreferrer" target="_blank" className={styles.LinkText}>Website</a>
-          <img
-            src={MajorArcanaIcon}
-            alt="Rotating cube that displays a gif on each side."
-            className={styles.ProjectIcon}
-          />
-          <a href="https://github.com/Mystechal-Divinators/project-week-tarot-divinators" rel="noreferrer" target="_blank" className={styles.LinkText}>GitHub</a>
+          <span className={styles.Extension}>
+            <a href="https://mystechal-divinators.github.io/project-week-tarot-divinators/" rel="noreferrer" target="_blank" className={styles.LinkText}>Website</a>
+          </span>
+          <span>| |</span>
+          <span className={styles.GitHub}>
+            <a href="https://github.com/Mystechal-Divinators/project-week-tarot-divinators" rel="noreferrer" target="_blank" className={styles.LinkText}>GitHub</a>
+          </span>
         </section>
       </main>
     </>
