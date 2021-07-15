d8e55f8e4162464a42d5684ec6bb56607904d249
Franco Ortega <francopdx@gmail.com>
2021-07-15 09:12:13 -0700

    resize ChooseGifDetails for all screen sizes

--- src/components/projects/ChooseGif/ChooseGifDetails.jsx
+++ src/components/projects/ChooseGif/ChooseGifDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import ClosePopup from '../../buttons/ClosePopup';
 import ProjectsDetails from '../../continents/Projects/ProjectsDetails';
 import Gif from './chooseGifData';
 import styles from './ChooseGifDetails.module.css';
@@ -9,26 +9,27 @@ const ChooseGifDetails = () => {
     <>
       <ProjectsDetails />
       <main className={styles.ChooseGifDetails}>
-        <section className={styles.ButtonContainer}>
-          <Link to="/projects">
-            <button className={styles.CloseButton} aria-label="Close">X</button>
-          </Link>
-        </section>
-        <h1>Choose GIF</h1>
+        <ClosePopup path={'projects'} />
+        <h1 className={styles.ProjectTitle}>Choose GIF</h1>
         <section className={styles.ProjectTech}>
             React | CSS | Node | Express | Jest | Giphy API | Heroku | Netlify
         </section>
         <section className={styles.ProjectInfo}>
             Browser-based app that displays real time trending GIFs, provides the ability to search for GIFs by category, and allows users to sign up and save GIFs. The majority of my efforts went into the responsive web design; the Sign Up and Sign In pages; parts of the functionality only available to signed in users; and testing the endpoints handled by our server.
         </section>
+        <img
+          src={Gif.currentGif}
+          alt="Rotating cube that displays a gif on each side."
+          className={styles.ProjectIcon}
+        />
         <section className={styles.ProjectLinks}>
-          <a href="https://choose-gif-fe.netlify.app/" rel="noreferrer" target="_blank" className={styles.LinkText}>Website</a>
-          <img
-            src={Gif.currentGif}
-            alt="Rotating cube that displays a gif on each side."
-            className={styles.ProjectIcon}
-          />
-          <a href="https://github.com/Choose-Gif" rel="noreferrer" target="_blank" className={styles.LinkText}>GitHub</a>
+          <span className={styles.Extension}>
+            <a href="https://choose-gif-fe.netlify.app/" rel="noreferrer" target="_blank" className={styles.LinkText}>Website</a>
+          </span>
+          <span>| |</span>
+          <span className={styles.GitHub}>
+            <a href="https://github.com/Choose-Gif" rel="noreferrer" target="_blank" className={styles.LinkText}>GitHub</a>
+          </span>
         </section>
       </main>
     </>
