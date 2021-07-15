10f3156ceafa9f31327918fff80bd6b8eda66fac
Franco Ortega <francopdx@gmail.com>
2021-07-15 11:40:40 -0700

    reformat links on EbbDetails

--- src/components/projects/Ebb/EbbDetails.jsx
+++ src/components/projects/Ebb/EbbDetails.jsx
@@ -24,18 +24,17 @@ const EbbDetails = () => {
         <section className={styles.ProjectInfo}>
         Chrome extension that reduces on-screen movement to increase website accessibility for users with sensory conditions. I removed GIFs from view, stopped CSS animations, injected our code into websites across all browser tabs, and implemented the styling provided by our designer. Built with a cross-functional team employing the agile methodology across several weekly sprints.
         </section>
-        
         <section className={styles.ProjectLinks}>
           <span className={styles.Extension}>
             <a href="https://chrome.google.com/webstore/detail/ebb/madmcmnfddadihndjcofieeeenbndlil" rel="noreferrer" target="_blank" className={styles.LinkText}>Extension</a>
           </span>
           <span>| |</span>
-          <span className={styles.GitHub}>
-            <a href="https://github.com/Co-Lab-You-Belong-in-Tech/ebb-extension" rel="noreferrer" target="_blank" className={styles.LinkText}>GitHub</a>
-          </span>
-          <span>| |</span>
           <span className={styles.Demo}>
             <a href="https://www.joincolab.io/projects/ebb" rel="noreferrer" target="_blank" className={styles.LinkText}>Demo</a>
+          </span>
+          <span>| |</span>
+          <span className={styles.GitHub}>
+            <a href="https://github.com/Co-Lab-You-Belong-in-Tech/ebb-extension" rel="noreferrer" target="_blank" className={styles.LinkText}>GitHub</a>
           </span>
         </section>
       </main>
