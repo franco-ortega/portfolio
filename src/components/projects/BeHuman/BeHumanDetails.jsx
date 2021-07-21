2b629ce8897d47c69b03b3d7432857a75e5cd3ff
Franco Ortega <francopdx@gmail.com>
2021-07-20 21:56:34 -0700

    update format of BeHumanDetails; adjust ClosePopup

--- src/components/projects/BeHuman/BeHumanDetails.jsx
+++ src/components/projects/BeHuman/BeHumanDetails.jsx
@@ -14,26 +14,28 @@ const BeHumanDetails = () => {
           background={'white'}
           color={'black'}
         />
-        <h1 className={styles.ProjectTitle}>beHuman</h1>
-        <section className={styles.ProjectTech}>
+        <section className={styles.Project}>
+          <h1 className={styles.ProjectTitle}>beHuman</h1>
+          <p className={styles.ProjectTech}>
             HTML | CSS | JavaScript | SQL | Node | Express | Supertest | Heroku
-        </section>
-        <section className={styles.ProjectInfo}>
+          </p>
+          <p className={styles.ProjectInfo}>
             VSCode extension that prompts users to take breaks with an hourly pop-up that provides wellness tips and movement videos. Users may adjust the time increment and reset the timer. My efforts included sandboxing to help develop the time tracking functionality and pop-ups; making a model with endpoints and testing; building an admin website to update the database and invite new admins.
-        </section>
-        <img
-          src={beHumanIcon}
-          alt="beHuman icon of a seated human with an open laptop for a head"
-          className={styles.ProjectIcon}
-        />
-        <section className={styles.ProjectLinks}>
-          <span className={styles.Extension}>
-            <a href="https://marketplace.visualstudio.com/items?itemName=not-bot.be-human" rel="noreferrer" target="_blank" className={styles.LinkText}>Extension</a>
-          </span>
-          <span>|</span>
-          <span className={styles.GitHub}>
-            <a href="https://github.com/alchemy-be-human" rel="noreferrer" target="_blank" className={styles.LinkText}>GitHub</a>
-          </span>
+          </p>
+          <p className={styles.ProjectLinks}>
+            <span className={styles.Extension}>
+              <a href="https://marketplace.visualstudio.com/items?itemName=not-bot.be-human" rel="noreferrer" target="_blank" className={styles.LinkText}>Extension</a>
+            </span>
+            <span>|</span>
+            <span className={styles.GitHub}>
+              <a href="https://github.com/alchemy-be-human" rel="noreferrer" target="_blank" className={styles.LinkText}>GitHub</a>
+            </span>
+          </p>
+          <img
+            src={beHumanIcon}
+            alt="beHuman icon of a seated human with an open laptop for a head"
+            className={styles.ProjectIcon}
+          />
         </section>
       </main>
     </>
