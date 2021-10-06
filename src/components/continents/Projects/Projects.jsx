be02ab8ed16be900e24d6034a52b06e21795ef2d
Franco Ortega <francopdx@gmail.com>
2021-10-06 15:40:02 -0700

    add outer-coastline mixin and apply to Projects continent

--- src/components/continents/Projects/Projects.jsx
+++ src/components/continents/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from './Projects.module.css';
+import styles from './Projects.module.scss';
 
 const Projects = () => {
   return (
