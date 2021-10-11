58e1aca8495e47d06568439f18cdb7c710ce06fc
Franco Ortega <francopdx@gmail.com>
2021-10-10 20:50:19 -0700

    change Legend module to scss and nest selectors

--- src/components/mapElements/Legend.jsx
+++ src/components/mapElements/Legend.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import styles from './Legend.module.css';
+import styles from './Legend.module.scss';
 
 const Legend = () => {
   return (
-    <section className={styles.LegendContainer}>
+    <section className={styles.Legend}>
       <div>
         <h3>Legend</h3>
         <article>
-          <p><span>Languages:</span> JavaScript, SQL, HTML, CSS</p>
+          <p><span>Languages:</span> JavaScript, HTML, CSS, SQL</p>
           <p><span>Front End:</span> React, Redux, Sass</p>
           <p><span>Back End:</span> Node, Express</p>
           <p><span>Databases:</span> PostgreSQL</p>
