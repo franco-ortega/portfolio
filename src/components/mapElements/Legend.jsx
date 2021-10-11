8b3bc2c6686f8fc5b83d5217d2e1e7214e898ec3
Franco Ortega <francopdx@gmail.com>
2021-10-10 18:25:56 -0700

    replace class names in Legend with selectors

--- src/components/mapElements/Legend.jsx
+++ src/components/mapElements/Legend.jsx
@@ -4,29 +4,17 @@ import styles from './Legend.module.css';
 const Legend = () => {
   return (
     <section className={styles.LegendContainer}>
-      <section className={styles.Legend}>
-        <h1 className={styles.Title}>Legend</h1>
-        <section className={styles.Tech}>
-          <p className={styles.TechEntry}>
-            <span className={styles.Bold}>Languages:</span> JavaScript, SQL, HTML, CSS
-          </p>
-          <p className={styles.TechEntry}>
-            <span className={styles.Bold}>Front End:</span> React, Redux, Sass
-          </p>
-          <p className={styles.TechEntry}>
-            <span className={styles.Bold}>Back End:</span> Node, Express
-          </p>
-          <p className={styles.TechEntry}>
-            <span className={styles.Bold}>Databases:</span> PostgreSQL
-          </p>
-          <p className={styles.TechEntry}>
-            <span className={styles.Bold}>Testing:</span> Jest, QUnit, React Testing Library, Supertest
-          </p>
-          <p className={styles.TechEntry}>
-            <span className={styles.Bold}>Tools:</span> Git, GitHub, Heroku, Miro, Netlify, pgAdmin, Postman, Slack, VSCode, Whimsical, Zoom
-          </p>
-        </section>
-      </section>
+      <div>
+        <h3>Legend</h3>
+        <article>
+          <p><span>Languages:</span> JavaScript, SQL, HTML, CSS</p>
+          <p><span>Front End:</span> React, Redux, Sass</p>
+          <p><span>Back End:</span> Node, Express</p>
+          <p><span>Databases:</span> PostgreSQL</p>
+          <p><span>Testing:</span> Jest, QUnit, React Testing Library, Supertest</p>
+          <p><span>Tools:</span> Git, GitHub, Heroku, Miro, Netlify, pgAdmin, Postman, Slack, VSCode, Whimsical, Zoom</p>
+        </article>
+      </div>
     </section>
   );
 };
