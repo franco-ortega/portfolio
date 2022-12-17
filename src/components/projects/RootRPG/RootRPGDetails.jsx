7dd78cac44935134e27b2bd9fe508477fe7ef6b3
Franco Ortega <francopdx@gmail.com>
2022-12-16 19:02:54 -0800

    add Root RPG content

--- src/components/projects/RootRPG/RootRPGDetails.jsx
+++ src/components/projects/RootRPG/RootRPGDetails.jsx
@@ -16,11 +16,24 @@ const RootRPGDetails = () => {
           <h1 className={styles.ProjectTitle}>Root RPG</h1>
           <p className={styles.ProjectTech}>NextJS | Sass</p>
           <p className={styles.ProjectInfo}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi fuga
-            impedit recusandae praesentium accusantium fugit repudiandae enim
-            facere sapiente? Magnam fuga vero dignissimos, unde iste
-            reprehenderit, ipsam eligendi ipsum possimus molestias voluptatibus
-            ullam cum fugiat dolorem. Aliquid, veniam. Ullam, ex.
+            As an avid board game and roleplaying game fan, I was delighted to
+            find out this summer that one of my favorite board games from over
+            the past several years was being adapted into a roleplaying game. I
+            built this app to help introduce the 200+ pages of rules in a
+            digestable format to new players.
+            <br />
+            <br />
+            The home page covers important terminology, and the other pages dive
+            deeper into the rules that will be used most often. The Playbooks
+            page also provides players with PDFs to download for all the
+            character classes.
+            <br />
+            <br />
+            As with many of my projects, I took a mobile-first approach and
+            focused on providing an intuitive interface that was paired with
+            proper guidance. Also, I selected colors and imagery to create a
+            vibe that matched the game. And it felt great to use my professional
+            skills to help bring joy to my personal life.
           </p>
           <ProjectLinks
             website={'https://root-rpg.vercel.app/'}
