645d18c078b290f836f769c2e8d63321f83fd4fa
Franco Ortega <francopdx@gmail.com>
2022-12-26 17:33:53 -0800

    replace ProfileDetails with ProfilePopUp

---
+++ src/components/continents/Profile/ProfilePopUp.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import photo from '../../../assets/FrancoOrtega.jpg';
+import ContinentPopUp from '../ContinentPopUp';
+import styles from './ProfilePopUp.module.scss';
+
+const ProfilePopUp = () => {
+  return (
+    <ContinentPopUp>
+      <header className={styles.Header}>
+        <img src={photo} alt='Franco Ortega photo' className={styles.Photo} />
+        <p className={styles.Name}>Franco Ortega</p>
+        <p className={styles.Contact}>
+          <a
+            href='mailto:francopdx@gmail.com'
+            rel='noreferrer'
+            target='_blank'
+            className={styles.ContactLink}
+          >
+            francopdx@gmail.com
+          </a>
+          <a href='tel:503-998-7444' className={styles.ContactLink}>
+            503-998-7444
+          </a>
+          <a
+            href='https://github.com/franco-ortega'
+            rel='noreferrer'
+            target='_blank'
+            className={styles.ContactLink}
+          >
+            GitHub
+          </a>
+          <a
+            href='https://www.linkedin.com/in/francoortega/'
+            rel='noreferrer'
+            target='_blank'
+            className={styles.ContactLink}
+          >
+            LinkedIn
+          </a>
+        </p>
+      </header>
+      <section className={styles.Bio}>
+        <span>
+          I&apos;m a software engineer who loves to create intuitive interfaces
+          and build out features across the stack. I appreciate well-documented
+          and efficient processes that offer structure and flexibility. I value
+          clear and kind communication, cross-functional collaboration, and
+          supportive environments that foster diversity and nurture growth.
+          Optimism guides my efforts while practicality ensures that my goals
+          are brought to fruition.
+        </span>
+        <span>
+          Additionally, I grew up very much a math and science kid, but I have
+          learned to love and nurture my creative side through writing,
+          cartography, and board game design. Other things that bring me joy are
+          bouldering, bike rides, art shows, and visiting friends who have
+          landed in faraway places. Also, roller hockey, punk rock, and zines
+          were big parts of my past that I very much appreciated and
+          occasionally still enjoy in smaller doses these days.
+        </span>
+        <span>
+          Yes to racial justice, social justice, climate justice, and economic
+          justice.
+        </span>
+      </section>
+    </ContinentPopUp>
+  );
+};
+
+export default ProfilePopUp;
