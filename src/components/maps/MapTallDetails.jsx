7979924ad7389865c1f4844079413f9ba4220879
Franco Ortega <francopdx@gmail.com>
2023-01-01 10:15:42 -0800

    refactor MapTallDetails to use ContinentPopUp

--- src/components/maps/MapTallDetails.jsx
+++ src/components/maps/MapTallDetails.jsx
@@ -2,26 +2,23 @@ import React from 'react';
 import ClosePopup from '../buttons/ClosePopup';
 import PropTypes from 'prop-types';
 import styles from './MapTallDetails.module.scss';
+import ContinentPopUp from '../continents/ContinentPopUp';
 
-const MapTallDetails = ({ mapTitle, map}) => {
+const MapTallDetails = ({ mapTitle, map }) => {
   return (
-    <div className={styles.MapTallDetails}>
-      <ClosePopup path={'cartography'} />
-      <p>{mapTitle}</p>
-      <img
-        alt={mapTitle}
-        className={styles.MapTall}
-        src={map}
-      />
-            
-    </div>
+    <ContinentPopUp>
+      <div className={styles.MapTallDetails}>
+        <ClosePopup path={'cartography'} />
+        <p>{mapTitle}</p>
+        <img alt={mapTitle} src={map} />
+      </div>
+    </ContinentPopUp>
   );
 };
 
 MapTallDetails.propTypes = {
   mapTitle: PropTypes.string.isRequired,
   map: PropTypes.string.isRequired
-
 };
 
 export default MapTallDetails;
