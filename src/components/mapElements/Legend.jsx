8aada98f58139731a46981b6418801d924f2a2e0
Franco Ortega <francopdx@gmail.com>
2023-01-09 17:40:39 -0800

    adjust font size across screen sizes

--- src/components/mapElements/Legend.jsx
+++ src/components/mapElements/Legend.jsx
@@ -8,8 +8,61 @@ const Legend = () => {
         <h3>Legend</h3>
         <div>
           <ul>
+            <li>HTML</li>
+          </ul>
+          <ul>
+            <li>CSS</li>
+            <li>Sass</li>
+          </ul>
+          <ul>
             <li>JavaScript</li>
-            <li>HTML/CSS</li>
+            <li>React</li>
+            <li>Redux</li>
+            <li>NextJS</li>
+          </ul>
+          <ul>
+            <li>Node</li>
+            <li>Express</li>
+            <li>GraphQL</li>
+            <li>Postman</li>
+          </ul>
+          <ul>
+            <li>Airtable</li>
+            <li>Mongo</li>
+            <li>PostgreSQL</li>
+            <li>Compass</li>
+            <li>pgAdmin</li>
+          </ul>
+          <ul>
+            <li>Jest</li>
+            <li>QUnit</li>
+            <li>Supertest</li>
+            <li>React Testing Library</li>
+          </ul>
+          <ul>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>Heroku</li>
+            <li>Netlify</li>
+          </ul>
+          <ul>
+            <li>Zapier</li>
+            <li>VSCode</li>
+          </ul>
+          <ul>
+            <li>Figma</li>
+            <li>Miro</li>
+            <li>Storybook</li>
+          </ul>
+          <ul>
+            <li>Asana</li>
+            <li>Hive</li>
+            <li>Jira</li>
+          </ul>
+          {/* <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
             <li>React</li>
             <li>Redux</li>
             <li>Sass</li>
@@ -41,8 +94,62 @@ const Legend = () => {
             <li>Asana</li>
             <li>Hive</li>
             <li>Jira</li>
-          </ul>
+          </ul> */}
         </div>
+        {/* <div>
+          <ul>
+            <li>HTML</li>
+          </ul>
+          <ul>
+            <li>CSS</li>
+            <li>Sass</li>
+          </ul>
+          <ul>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Redux</li>
+            <li>NextJS</li>
+          </ul>
+          <ul>
+            <li>Node</li>
+            <li>Express</li>
+            <li>GraphQL</li>
+            <li>Postman</li>
+          </ul>
+          <ul>
+            <li>Mongo</li>
+            <li>Compass</li>
+          </ul>
+          <ul>
+            <li>PostgreSQL</li>
+            <li>pgAdmin</li>
+            <li>Airtable</li>
+          </ul>
+          <ul>
+            <li>Jest</li>
+            <li>QUnit</li>
+            <li>Supertest</li>
+            <li>React Testing Library</li>
+          </ul>
+          <ul>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>Heroku</li>
+            <li>Netlify</li>
+            <li>Zapier</li>
+          </ul>
+          <ul>
+            <li>VSCode</li>
+          </ul>
+          <ul>
+            <li>Figma</li>
+            <li>Miro</li>
+            <li>Storybook</li>
+            <li>Asana</li>
+            <li>Hive</li>
+            <li>Jira</li>
+          </ul>
+        </div> */}
       </div>
     </section>
   );
