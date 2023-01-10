f2021b895e2b2c176f34a0bc15accef64d33a430
Franco Ortega <francopdx@gmail.com>
2023-01-09 18:48:05 -0800

    adjust grid to keep Legend contained across screen sizes

--- src/components/mapElements/Legend.jsx
+++ src/components/mapElements/Legend.jsx
@@ -37,7 +37,7 @@ const Legend = () => {
             <li>Jest</li>
             <li>QUnit</li>
             <li>Supertest</li>
-            <li>React Testing Library</li>
+            <li>React TL</li>
           </ul>
           <ul>
             <li>Git</li>
@@ -59,97 +59,7 @@ const Legend = () => {
             <li>Hive</li>
             <li>Jira</li>
           </ul>
-          {/* <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>React</li>
-            <li>Redux</li>
-            <li>Sass</li>
-            <li>NextJS</li>
-            <li>Node</li>
-            <li>Express</li>
-            <li>GraphQL</li>
-            <li>Mongo</li>
-            <li>PostgreSQL</li>
-            <li>Airtable</li>
-            <li>Jest</li>
-            <li>QUnit</li>
-            <li>Supertest</li>
-            <li>React Testing Library</li>
-          </ul>
-          <ul>
-            <li>Compass</li>
-            <li>pgAdmin</li>
-            <li>Git</li>
-            <li>GitHub</li>
-            <li>Heroku</li>
-            <li>Netlify</li>
-            <li>Postman</li>
-            <li>Zapier</li>
-            <li>VSCode</li>
-            <li>Figma</li>
-            <li>Miro</li>
-            <li>Storybook</li>
-            <li>Asana</li>
-            <li>Hive</li>
-            <li>Jira</li>
-          </ul> */}
         </div>
-        {/* <div>
-          <ul>
-            <li>HTML</li>
-          </ul>
-          <ul>
-            <li>CSS</li>
-            <li>Sass</li>
-          </ul>
-          <ul>
-            <li>JavaScript</li>
-            <li>React</li>
-            <li>Redux</li>
-            <li>NextJS</li>
-          </ul>
-          <ul>
-            <li>Node</li>
-            <li>Express</li>
-            <li>GraphQL</li>
-            <li>Postman</li>
-          </ul>
-          <ul>
-            <li>Mongo</li>
-            <li>Compass</li>
-          </ul>
-          <ul>
-            <li>PostgreSQL</li>
-            <li>pgAdmin</li>
-            <li>Airtable</li>
-          </ul>
-          <ul>
-            <li>Jest</li>
-            <li>QUnit</li>
-            <li>Supertest</li>
-            <li>React Testing Library</li>
-          </ul>
-          <ul>
-            <li>Git</li>
-            <li>GitHub</li>
-            <li>Heroku</li>
-            <li>Netlify</li>
-            <li>Zapier</li>
-          </ul>
-          <ul>
-            <li>VSCode</li>
-          </ul>
-          <ul>
-            <li>Figma</li>
-            <li>Miro</li>
-            <li>Storybook</li>
-            <li>Asana</li>
-            <li>Hive</li>
-            <li>Jira</li>
-          </ul>
-        </div> */}
       </div>
     </section>
   );
