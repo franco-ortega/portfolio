3460480310ec8a4cb5853a4fe8882da7df876f1b
Franco Ortega <francopdx@gmail.com>
2024-04-16 16:20:47 -0700

    create Border component to house map border and hold Ocean

--- src/components/mapElements/border/Border.jsx
+++ src/components/mapElements/border/Border.jsx
@@ -1,30 +1,9 @@
 import React from 'react';
-import BorderCorner from './BorderCorner';
 import PropTypes from 'prop-types';
-import { borderCornerData } from './borderCornerData';
+import styles from './Border.module.scss';
 
 const Border = ({ children }) => {
-  return (
-    <>
-      <BorderCorner
-        grid={borderCornerData.cornerNW.grid}
-        shape={borderCornerData.cornerNW.shape}
-      />
-      <BorderCorner
-        grid={borderCornerData.cornerNE.grid}
-        shape={borderCornerData.cornerNE.shape}
-      />
-      <BorderCorner
-        grid={borderCornerData.cornerSW.grid}
-        shape={borderCornerData.cornerSW.shape}
-      />
-      <BorderCorner
-        grid={borderCornerData.cornerSE.grid}
-        shape={borderCornerData.cornerSE.shape}
-      />
-      <div>{children}</div>
-    </>
-  );
+  return <div className={styles.Border}>{children}</div>;
 };
 
 Border.propTypes = {
