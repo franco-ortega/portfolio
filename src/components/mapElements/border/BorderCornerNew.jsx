394c8e8a3dcfe9a18eecb6baa515a3f7247f39ce
Franco Ortega <francopdx@gmail.com>
2024-04-17 11:47:03 -0700

    create new border corner component that uses position absolute

---
+++ src/components/mapElements/border/BorderCornerNew.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import styles from './BorderCornerNew.module.scss';
+
+function BorderCornerNew() {
+  return <div className={styles.BorderCornerNew}></div>;
+}
+
+BorderCornerNew.propTypes = {};
+
+export default BorderCornerNew;
