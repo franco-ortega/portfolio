1b0ff8455f4de37406351ae6ea82356274bfdee8
Franco Ortega <francopdx@gmail.com>
2024-04-17 16:18:38 -0700

    move linear gradient into a function for ease and clarity

--- src/components/mapElements/border/Border.jsx
+++ src/components/mapElements/border/Border.jsx
@@ -4,51 +4,45 @@ import BorderCorner from './BorderCorner';
 import styles from './Border.module.scss';
 
 const Border = ({ children }) => {
+  const borderCornerGradient = (direction) => {
+    return `linear-gradient(
+      to ${direction},
+      hsl(240, 60%, 25%),
+      hsl(0, 0%, 95%),
+      hsl(240, 60%, 25%)
+    )`;
+  };
+
+  const direction = {
+    TOP_RIGHT: 'top right',
+    TOP_LEFT: 'top left',
+  };
+
   return (
     <div className={styles.Border}>
       <BorderCorner
         customStyles={{
-          background: `linear-gradient(
-            to top right,
-            hsl(240, 60%, 25%),
-            hsl(0, 0%, 95%),
-            hsl(240, 60%, 25%)
-          )`,
+          background: borderCornerGradient(direction.TOP_RIGHT),
           borderRadius: '0% 0% 90% 0%',
         }}
       />
       <BorderCorner
         customStyles={{
-          background: `linear-gradient(
-            to top left,
-            hsl(240, 60%, 25%),
-            hsl(0, 0%, 95%),
-            hsl(240, 60%, 25%)
-          )`,
+          background: borderCornerGradient(direction.TOP_LEFT),
           borderRadius: '0% 0% 0% 90%',
           justifySelf: 'flex-end',
         }}
       />
       <BorderCorner
         customStyles={{
-          background: `linear-gradient(
-            to top left,
-            hsl(240, 60%, 25%),
-            hsl(0, 0%, 95%),
-            hsl(240, 60%, 25%)
-          )`,
+          background: borderCornerGradient(direction.TOP_LEFT),
           borderRadius: '0% 90% 0% 0%',
           alignSelf: 'flex-end',
         }}
       />
       <BorderCorner
         customStyles={{
-          background: `linear-gradient(
-            to top right,
-            hsl(240, 60%, 25%),
-            hsl(0, 0%, 95%),
-            hsl(240, 60%, 25%)
-          )`,
+          background: borderCornerGradient(direction.TOP_RIGHT),
           borderRadius: '90% 0% 0% 0%',
           alignSelf: 'flex-end',
           justifySelf: 'flex-end',
