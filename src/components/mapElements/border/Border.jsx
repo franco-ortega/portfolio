4e03c318863f8dc86f7d7acef6ad4c92901c2718
Franco Ortega <francopdx@gmail.com>
2024-04-17 16:05:57 -0700

    move linear gradient for BorderCorner into inline style to individually customize direction

--- src/components/mapElements/border/Border.jsx
+++ src/components/mapElements/border/Border.jsx
@@ -6,15 +6,49 @@ import styles from './Border.module.scss';
 const Border = ({ children }) => {
   return (
     <div className={styles.Border}>
-      <BorderCorner customStyles={{ borderRadius: '0% 0% 90% 0%' }} />
       <BorderCorner
-        customStyles={{ borderRadius: '0% 90% 0% 0%', alignSelf: 'flex-end' }}
-      />
-      <BorderCorner
-        customStyles={{ borderRadius: '0% 0% 0% 90%', justifySelf: 'flex-end' }}
+        customStyles={{
+          background: `linear-gradient(
+            to top right,
+            hsl(240, 60%, 25%),
+            hsl(0, 0%, 95%),
+            hsl(240, 60%, 25%)
+          )`,
+          borderRadius: '0% 0% 90% 0%',
+        }}
       />
       <BorderCorner
         customStyles={{
+          background: `linear-gradient(
+            to top left,
+            hsl(240, 60%, 25%),
+            hsl(0, 0%, 95%),
+            hsl(240, 60%, 25%)
+          )`,
+          borderRadius: '0% 0% 0% 90%',
+          justifySelf: 'flex-end',
+        }}
+      />
+      <BorderCorner
+        customStyles={{
+          background: `linear-gradient(
+            to top left,
+            hsl(240, 60%, 25%),
+            hsl(0, 0%, 95%),
+            hsl(240, 60%, 25%)
+          )`,
+          borderRadius: '0% 90% 0% 0%',
+          alignSelf: 'flex-end',
+        }}
+      />
+      <BorderCorner
+        customStyles={{
+          background: `linear-gradient(
+            to top right,
+            hsl(240, 60%, 25%),
+            hsl(0, 0%, 95%),
+            hsl(240, 60%, 25%)
+          )`,
           borderRadius: '90% 0% 0% 0%',
           alignSelf: 'flex-end',
           justifySelf: 'flex-end',
