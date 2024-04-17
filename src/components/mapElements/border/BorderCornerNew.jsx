668cbeeb957e8b525af685b0bfd6db0b8265c184
Franco Ortega <francopdx@gmail.com>
2024-04-17 14:05:01 -0700

    delete deprecated files

--- src/components/mapElements/border/BorderCornerNew.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './BorderCornerNew.module.scss';
-
-function BorderCornerNew() {
-  return <div className={styles.BorderCornerNew}></div>;
-}
-
-export default BorderCornerNew;
