4f8ef4efa87454f786b2d4c94b61e3303b23a1b0
Franco Ortega <francopdx@gmail.com>
2024-05-07 20:50:10 -0700

    apply more narrow tablet view to CartographyPopUp

--- src/components/continents/Cartography/CartographyPopUp.jsx
+++ src/components/continents/Cartography/CartographyPopUp.jsx
@@ -19,49 +19,51 @@ const CartographyPopUp = () => {
         <header>
           <h2>Cartography</h2>
         </header>
-        <section>
-          <p>
-            The inspiration for this site came from my longtime love of fantasy
-            cartography. From <span>Middle Earth</span> to <span>Westeros</span>{' '}
-            to the <span>Grishaverse</span>, I&apos;ve always loved flipping to
-            the maps in fantasy novels whenever a new location is mentioned, and
-            in recent years, have started creating my own worlds with pen and
-            paper.
-          </p>
-          <p>Click images to see full maps.</p>
-        </section>
-        <section>
-          <Link to='/april'>
-            <div className={styles.April}>
-              <span role='img' aria-label='April map'></span>
-            </div>
-          </Link>
-          <Link to='/ganderlund'>
-            <div className={styles.Ganderlund}>
-              <span role='img' aria-label='Ganderlund map'></span>
-            </div>
-          </Link>
-          <Link to='/gruenne'>
-            <div className={styles.Gruenne}>
-              <span role='img' aria-label='Gruenne map'></span>
-            </div>
-          </Link>
-          <Link to='/harland'>
-            <div className={styles.Harland}>
-              <span role='img' aria-label='Harland map'></span>
-            </div>
-          </Link>
-          <Link to='/marneosso'>
-            <div className={styles.MarneOsso}>
-              <span role='img' aria-label='Marne Osso map'></span>
-            </div>
-          </Link>
-          <Link to='/strigi'>
-            <div className={styles.Strigi}>
-              <span role='img' aria-label='Strigi map'></span>
-            </div>
-          </Link>
-        </section>
+        <div>
+          <section>
+            <p>
+              The inspiration for this site came from my longtime love of
+              fantasy cartography. From <span>Middle Earth</span> to{' '}
+              <span>Westeros</span> to the <span>Grishaverse</span>, I&apos;ve
+              always loved flipping to the maps in fantasy novels whenever a new
+              location is mentioned, and in recent years, have started creating
+              my own worlds with pen and paper.
+            </p>
+            <p>Click images to see full maps.</p>
+          </section>
+          <section>
+            <Link to='/april'>
+              <div className={styles.April}>
+                <span role='img' aria-label='April map'></span>
+              </div>
+            </Link>
+            <Link to='/ganderlund'>
+              <div className={styles.Ganderlund}>
+                <span role='img' aria-label='Ganderlund map'></span>
+              </div>
+            </Link>
+            <Link to='/gruenne'>
+              <div className={styles.Gruenne}>
+                <span role='img' aria-label='Gruenne map'></span>
+              </div>
+            </Link>
+            <Link to='/harland'>
+              <div className={styles.Harland}>
+                <span role='img' aria-label='Harland map'></span>
+              </div>
+            </Link>
+            <Link to='/marneosso'>
+              <div className={styles.MarneOsso}>
+                <span role='img' aria-label='Marne Osso map'></span>
+              </div>
+            </Link>
+            <Link to='/strigi'>
+              <div className={styles.Strigi}>
+                <span role='img' aria-label='Strigi map'></span>
+              </div>
+            </Link>
+          </section>
+        </div>
       </FocusOn>
     </PopUp>
   );
