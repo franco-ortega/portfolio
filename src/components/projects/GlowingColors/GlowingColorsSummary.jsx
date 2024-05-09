99d388998a9a95c1657949830d30c9f8d9d02e78
Franco Ortega <francopdx@gmail.com>
2024-05-09 13:24:52 -0700

    resolve import conflict

--- src/components/projects/GlowingColors/GlowingColorsSummary.jsx
+++ src/components/projects/GlowingColors/GlowingColorsSummary.jsx
@@ -25,3 +25,31 @@ const GlowingColorsSummary = () => {
 };
 
 export default GlowingColorsSummary;
+
+import React from 'react';
+import ProjectSummary from '../ProjectSummary';
+import ProjectLinks from '../../links/ProjectLinks';
+import OpenDetails from '../../buttons/OpenDetails';
+
+const GlowingColorsSummary = () => {
+  return (
+    <ProjectSummary>
+      <div>
+        <h2>Glowing Colors</h2>
+        <div>
+          <p>
+            A colorful visual experience that allows the user to enjoy a
+            peaecful, shifting, and interactive universe.
+          </p>
+          <ProjectLinks
+            website={'https://glowing-colors.netlify.app/'}
+            github={'https://github.com/franco-ortega/glowing-colors'}
+          />
+        </div>
+        <OpenDetails path={'ebb'} />
+      </div>
+    </ProjectSummary>
+  );
+};
+
+export default GlowingColorsSummary;
