14f6fef3c11c2692eef306f4ec632960f67e97d0
Franco Ortega <francopdx@gmail.com>
2024-05-09 14:58:29 -0700

    add copy to GlowingColorsDetails

--- src/components/projects/GlowingColors/GlowingColorsDetails.jsx
+++ src/components/projects/GlowingColors/GlowingColorsDetails.jsx
@@ -10,9 +10,16 @@ const GlowingColorsDetails = () => {
       <div className={styles.GlowingColorsDetails}>
         <ClosePopup path={'projects'} background={'black'} color={'white'} />
         <h3>Glowing Colors</h3>
-        <h4>React | JavaScript | HTML| CSS</h4>
-        <p>CONTENT - ABOUT</p>
-        <p>CONTENT - TECH</p>
+        <h4>React | JavaScript | HTML | CSS</h4>
+        <p>
+          A colorful and dynamic visual experience that engages the user in a
+          peaecful, shifting, and interactive universe.
+        </p>
+        <p>
+          The app repeatedly renders multiple colorful orbs of varying hues and
+          sizes. When the user clicks on an orb, it runs an algorithm that
+          shifts the location and changes the styles of the selected orb.
+        </p>
         <ProjectLinks
           website={'https://glowing-colors.netlify.app'}
           github={'https://github.com/franco-ortega/glowing-colors'}
